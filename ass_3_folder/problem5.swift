46a9d7081df3fc918ad800901e339ec4cb2413cb
ksnbkvais <114652308+ksnbkvais@users.noreply.github.com>
2024-10-03 23:23:15 +0500

    Update problem5.swift

--- ass_3_folder/problem5.swift
+++ ass_3_folder/problem5.swift
@@ -1,1 +1,22 @@
 
+import Foundation
+
+func wordfrek (_ sentence: String) -> [String : Int] {
+    
+    let newSentence = sentence.lowercased().components(separatedBy: CharacterSet.punctuationCharacters).joined()
+    let words = newSentence.split(separator: " ")
+    var wordFrequincy : [String : Int] = [:]
+    
+    for word in words {
+        let wordF = String(word)
+        wordFrequincy[wordF, default: 0] += 1
+    }
+    return wordFrequincy
+}
+
+let primer = "This is example for print. She is in love with someone who is really good example of red flag"
+let frq = wordfrek(primer)
+
+for (word, count) in frq {
+    print("'\(word)' : \(count)")
+}
